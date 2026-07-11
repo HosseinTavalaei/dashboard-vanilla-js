334591d8a0ae00d1462e3defa1e2d00548c1e7ee
Hossein tavalaei <greathosseinchi@gmail.com>
2026-07-11 22:51:40 +0330

    install sass loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -19,4 +19,19 @@ export default {
     compress: true,
     port: 9000,
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 };
